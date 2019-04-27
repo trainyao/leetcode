00f6a2d9ad8db730c6d023ec8192d4b4fed00a3d
trainyao <rcy@ddpkcc.com>
2019-04-27 10:51:12 +0800

    快排

---
+++ quick-sort/go/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	res, _ := quickSort([]int{2, 1, 1, 1})
+	log.Printf("%a", res)
+	res, _ = quickSort([]int{1, 2, 1, 1})
+	log.Printf("%a", res)
+	res, _ = quickSort([]int{5, 1, 8, 9, 10, 4, 3, 2, 2, 1})
+	log.Printf("%a", res)
+	res, _ = quickSort([]int{1, 1, 1})
+	log.Printf("%a", res)
+	res, _ = quickSort([]int{1})
+	log.Printf("%a", res)
+	res, _ = quickSort([]int{})
+	log.Printf("%a", res)
+}
+
+func quickSort(a []int) ([]int, error) {
+	if len(a) <= 1 {
+		return a, nil
+	}
+
+	benchmarkValueIndex := 0
+	benchmarkValue := a[benchmarkValueIndex]
+	for index, value := range a {
+		// 跨过第一个元素
+		if index == 0 {
+			continue
+		}
+
+		if value < benchmarkValue {
+			// 如果后面的元素小于第一个元素 交换位置
+			a[index], a[benchmarkValueIndex] = a[benchmarkValueIndex], a[index]
+
+			// 保持第一个元素前面的都是小于它的元素 后面都是大于它的元素
+			// 如果发现交换后放到了较大元素的后面,于是和较大元素的第一个交换一下位置
+			// 如: 基准元素=5, 1[5]987[2]3 => 12[9]87[5]3 => 12[5]87[9]3
+			if benchmarkValueIndex+1 < len(a) && benchmarkValueIndex+1 != index {
+				a[benchmarkValueIndex+1], a[index] = a[index], a[benchmarkValueIndex+1]
+				benchmarkValueIndex = benchmarkValueIndex + 1
+			} else {
+				benchmarkValueIndex = index
+			}
+		}
+	}
+
+	// 迭代前面部分
+	_, err := quickSort(a[:benchmarkValueIndex])
+	if err != nil {
+		return a, err
+	}
+	// 迭代后面部分
+	if benchmarkValueIndex+1 < len(a) {
+		_, err := quickSort(a[benchmarkValueIndex+1:])
+		if err != nil {
+			return a, err
+		}
+	}
+
+	return a, nil
+}
