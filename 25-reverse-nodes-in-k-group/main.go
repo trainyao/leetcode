0dbfb981a30d9e5f29cd42669529b7ccaec64683
yaochangyu <yaochangyu@youmi.net>
2020-04-26 12:56:05 +0800

    25-reverse-nodes-in-k-group

--- 25-reverse-nodes-in-k-group/main.go
+++ 25-reverse-nodes-in-k-group/main.go
@@ -5,26 +5,31 @@ import "log"
 func main() {
 	head := &ListNode{
 		Val: 1,
-		Next: nil,
-		//Next: &ListNode{
-		//	Val: 2,
-		//	Next: nil,
-		//	//Next: &ListNode{
-		//	//	Val:  3,
-		//	//	Next: nil,
-		//	//	//Next: &ListNode{
-		//	//	//	Val:  4,
-		//	//	//	Next: nil,
-		//	//	//	//Next: &ListNode{
-		//	//	//	//	Val:  5,
-		//	//	//	//	Next: nil,
-		//	//	//	//},
-		//	//	//},
-		//	//},
-		//},
+		//Next: nil,
+		Next: &ListNode{
+			Val: 2,
+			//Next: nil,
+			Next: &ListNode{
+				Val:  3,
+				//Next: nil,
+				Next: &ListNode{
+					Val:  4,
+					//Next: nil,
+					Next: &ListNode{
+						Val:  5,
+						Next: nil,
+						//Next: &ListNode{
+						//	Val:  6,
+						//	Next: nil,
+						//},
+					},
+				},
+			},
+		},
 	}
 
-	head = swapPairs(head)
+	head = reverseKGroup(head, 1)
+
 	for true {
 		log.Println("%d", head.Val)
 
@@ -41,70 +46,81 @@ type ListNode struct {
 	Next *ListNode
 }
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-
-/*
-1.next = 4
-2.next = 1
-0.next = 2
-
-2.next = 1
-0.next = 2
-4.next = 3
-*/
-
-func swapPairs(head *ListNode) *ListNode {
-	if head == nil {
-		return nil
-	}
-	if head.Next == nil {
+func reverseKGroup(head *ListNode, k int) (ret *ListNode) {
+	if k == 1 {
 		return head
 	}
 
-	pcurrent := head
-	log := map[int]*ListNode{}
-	// special node
-	log[0] = &ListNode{}
+	var counter int
+	var ptr *ListNode
 
-	counter := 1
-	for true {
-		pnext := pcurrent.Next
-
-		log[counter]=pcurrent
-
-		if counter % 2 == 1{
-			index := counter - 2
-			if index <= 0 {
-				index = 0
+	ptr = head
+	n := 0
+	counter = 1
+	start := head
+	end := head
+	var tmp *ListNode
+	for {
+		if ptr.Next == nil || counter == k {
+			if ptr.Next == nil && counter != k {
+				end.Next = start
+				break
 			}
 
-			log[index].Next = pcurrent.Next
+			// store 4
+			tmp = ptr.Next
 
-			if pnext == nil {
-				log[index].Next = pcurrent
-			}
-		} else {
-			index := counter - 1
-			log[counter].Next = log[index]
+			ptr.Next = nil
 
-			if pnext == nil {
-				log[index].Next = nil
+			h, t := revertList(start, counter)
+			if n == 0 {
+				ret = h
+			} else {
+				end.Next = h
 			}
+			end = t
+
+			// if last link
+			if tmp == nil {
+				break
+			}
+
+			// set ptr to 4, begin to count again
+			ptr = tmp
+			start = tmp
+			// reset counter
+			counter = 1
+			n++
+			continue
 		}
 
-		if pnext == nil {
-			break
-		} else {
-			pcurrent = pnext
-			counter++
+		counter++
+		ptr = ptr.Next
+	}
+
+	return
+}
+
+func revertList(l *ListNode, len int) (head *ListNode, tail *ListNode) {
+	if l.Next == nil {
+		return l, l
+	}
+
+	tail = l
+
+	a := l
+	b := l.Next
+	l.Next = nil
+
+	for i := 1; i < len; i++ {
+		tmp := b.Next
+		b.Next = a
+		a = b
+		if tmp != nil {
+			b = tmp
 		}
 	}
 
-	return log[0].Next
+	head = b
+	return
 }
