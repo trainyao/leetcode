874928d4d5eb1d7af4ebbaa3194f477134960893
yaochangyu <yaochangyu@youmi.net>
2020-06-15 20:15:22 +0800

    1129-Shortest-Path-with-Alternating-Colors tmp

---
+++ 1129-Shortest-Path-with-Alternating-Colors/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+
+}
+/*
+
+if 0->x > 0->x-1 + currentStart -> currentEnd && colorMatch()
+*/
+
+func shortestAlternatingPaths(n int, red_edges [][]int, blue_edges [][]int) []int {
+	blueStart := make([]int, n)
+	redStart := make([]int, n)
+
+	for 
+
+
+
+}
